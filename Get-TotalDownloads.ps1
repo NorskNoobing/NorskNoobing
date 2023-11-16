a4e07e9aa51d579190691018520de4686ba4a8ee
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2023-11-16 14:36:17 +0100

    Update Get-TotalDownloads.ps1
    * Use comments to insert badge

--- Get-TotalDownloads.ps1
+++ Get-TotalDownloads.ps1
@@ -1,23 +1,27 @@
 param (
-    [string]$PsgalleryUsername = "NorskNoobing",
-    [string]$LinkStr = "https://img.shields.io/badge/PSGallery%20Downloads-"
+    [string]$PSGalleryLink = "https://www.powershellgallery.com/profiles/NorskNoobing"
 )
+
+#Returns bool if "README.md" contains badge field or not
+$BadgeExists = (Get-Content "README.md" | Select-String "<!-- PSGallery-Downloads:START -->") -and (Get-Content "README.md" | Select-String "<!-- PSGallery-Downloads:END -->")
+
+if (!$BadgeExists) {
+    Write-Error "Badge field has to be added to your README before running workflow."
+}
 
 Install-Module "PowerHTML" -Force
 
 #Scrape PSGallery profile for the "Total Downloads" property
-$PsgalleryProfile = ((Invoke-WebRequest -Uri "https://www.powershellgallery.com/profiles/$PsgalleryUsername" | ConvertFrom-Html).InnerText -split '\r?\n').Trim()
+$PsgalleryProfile = ((Invoke-WebRequest -Uri $PSGalleryLink | ConvertFrom-Html).InnerText -split '\r?\n').Trim()
 $TotalDownloadsIndex = $PsgalleryProfile.IndexOf("Total downloads of packages")
 $PsgalleryTotalDownloads = $PsgalleryProfile[$TotalDownloadsIndex-2]
 
-#Returns bool if "README.md" contains badge or not
-$LinkMatchRegex = "$($LinkStr.replace(".","\.").replace("/","\/"))[0-9]*"
-$BadgeExists = Get-Content "README.md" | Select-String -pattern $LinkMatchRegex
+#See LinkStr options here https://shields.io/badges/static-badge
+$LinkStr = "https://img.shields.io/badge/PSGallery%20Downloads-$PsgalleryTotalDownloads-blue?style=flat-square&logo=powershell"
 
-if ($BadgeExists) {
-    #Update badge
-    (Get-Content "README.md") -Replace "$LinkMatchRegex",
-    "$LinkStr$PsgalleryTotalDownloads" | Out-File "README.md"
-} else {
-    Write-Error "Badge has to be added to your README before running workflow."
-}
+#Update badge
+(Get-Content "README.md") -Replace @"
+<!-- PSGallery-Downloads:START -->.*<!-- PSGallery-Downloads:END -->
+"@,@"
+<!-- PSGallery-Downloads:START -->[![]($LinkStr)]($PSGalleryLink)<!-- PSGallery-Downloads:END -->
+"@ | Out-File "README.md"
